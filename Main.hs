b635a0c4b981a23c08fbef3e3348b4225a3413a2
gaprop <anders.kildemand@gmail.com>
2022-01-31 14:46:15 +0100

    Finished project although can be made better

--- Main.hs
+++ Main.hs
@@ -8,31 +8,41 @@ import System.Console.ANSI
 
 main = do
   args <- getArgs
+  print args
   withFile (args !! 0) ReadMode (\handle0 -> do
     withFile (args !! 1) ReadMode (\handle1 -> do
       guesses <- hGetContents handle0
       answers <- hGetContents handle1
       let guesses' = parse parseResult "Guesses" guesses
           answers' = parse parseAnswers "Answers" answers
-      print "Hello"
-       -- in case (guesses', answers') of
-        -- (Right pGuesses, Right pAnswers) -> print $ run pGuesses pAnswers
+       in case (guesses', answers') of
+        (Right pGuesses, Right pAnswers) -> run pGuesses pAnswers
       )
     )
 
-run :: [(Declension, [Guess])] -> [[Answer]] -> String
-run guesses answers = map (\(Left (wrong, right), Right (s) -> "Hello") compared
-  where compared = compareParsed guesses answers
+run :: [(Declension, [Guess])] -> [[Answer]] -> IO ()
+run guesses answers =
+  let compared = compareParsed guesses answers
+      ansi     = map (unwords . map showParsed) compared
+   in mapM_ putStrLn ansi
+  where showParsed s = case s of 
+                         Right x -> x
+                         Left (wrong, right) -> setSGRCode [SetColor Foreground Vivid Red] ++ wrong ++ "(" ++ right ++")" ++ setSGRCode []
+
    
 
-compareParsed :: [(Declension, [Guess])] -> [[Answer]] -> [Either (String, String) String]
-compareParsed guesses answers = flatten . map (map compareParsed') . wrap $ ((map snd guesses), answers)
+compareParsed :: [(Declension, [Guess])] -> [[Answer]] -> [[Either (String, String) String]]
+compareParsed guesses answers = map (map compareParsed') . wrap $ fill (map snd guesses) answers
   where 
-    flatten = concat
     compareParsed' :: (Guess, Answer) -> Either (String, String) String
     compareParsed' ((Guess x), (Answer y)) = if x == y 
                                                then Right x
                                                else Left (x, y)
+    fill :: [[Guess]] -> [[Answer]] -> ([[Guess]], [[Answer]])
+    fill x y = 
+      let x' = map fill' $ zip x y
+       in (x', y)
+      where fill' (a, b) = a ++ (take (length b - length a) $ repeat (Guess "_"))
 
     wrap :: ([[a]], [[b]]) -> [[(a, b)]]
     wrap (x, y) = map (\(x, y) -> zip x y) $ zip x y
